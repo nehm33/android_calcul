8fc52e4faad13bfec726a288db18ffce4224d933
Emmanuel YAO <nehm33@gmail.com>
2024-03-11 17:03:32 +0100

    Add options fragment user interface

---
+++ app/src/main/java/com/platydev/calculmental/ui/OptionsFragment.java
@@ -0,0 +1,40 @@
+package com.platydev.calculmental.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.platydev.calculmental.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OptionsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OptionsFragment extends Fragment {
+
+    public OptionsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static OptionsFragment newInstance() {
+        return new OptionsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_options, container, false);
+    }
+}
