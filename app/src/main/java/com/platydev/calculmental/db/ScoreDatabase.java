9d9ef4362d364cb3cb2f1bc8e4016196a63dc865
Emmanuel YAO <nehm33@gmail.com>
2024-03-27 21:47:34 +0100

    Finalize database classes

--- app/src/main/java/com/platydev/calculmental/db/ScoreDatabase.java
+++ app/src/main/java/com/platydev/calculmental/db/ScoreDatabase.java
@@ -2,11 +2,14 @@ package com.platydev.calculmental.db;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.platydev.calculmental.data.score.Score;
 import com.platydev.calculmental.db.dao.ScoreDAO;
 
+@Database(entities = {Score.class}, version = 1, exportSchema = false)
 public abstract class ScoreDatabase extends RoomDatabase {
 
     private static volatile ScoreDatabase INSTANCE;
--- app/src/main/java/com/platydev/calculmental/db/dao/ScoreDAO.java
+++ app/src/main/java/com/platydev/calculmental/db/dao/ScoreDAO.java
@@ -12,16 +12,16 @@ import java.util.List;
 @Dao
 public interface ScoreDAO {
 
-    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode AND temps = :temps")
+    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode AND temps = :temps ORDER BY score DESC, date ASC")
     List<Score> getScores(int niveau, String mode, long temps);
 
-    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode")
+    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode ORDER BY score DESC, temps ASC, date ASC")
     List<Score> getNotFixedTimeModeScores(int niveau, String mode);
 
-    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode AND temps = :temps ORDER BY score DESC, date ASC LIMIT 1")
+    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode AND temps = :temps ORDER BY score ASC, date DESC LIMIT 1")
     Score getWorstScore(int niveau, String mode, long temps);
 
-    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode ORDER BY score DESC, temps ASC, date ASC LIMIT 1")
+    @Query("SELECT * FROM scores WHERE niveau = :niveau AND mode = :mode ORDER BY score ASC, temps DESC, date DESC LIMIT 1")
     Score getNotFixedTimeModeWrostScore(int niveau, String mode);
 
     @Insert
