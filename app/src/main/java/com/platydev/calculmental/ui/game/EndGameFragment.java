1ac1d7ef5cbcdc5534e35ca87e374e96ecd22689
Emmanuel YAO <nehm33@gmail.com>
2024-03-29 17:38:37 +0100

    Modify game fragment and viewModel to add high scores

--- app/src/main/java/com/platydev/calculmental/ui/game/EndGameFragment.java
+++ app/src/main/java/com/platydev/calculmental/ui/game/EndGameFragment.java
@@ -3,23 +3,19 @@ package com.platydev.calculmental.ui.game;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.text.InputType;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-
 import com.platydev.calculmental.R;
-import com.platydev.calculmental.data.options.Options;
-import com.platydev.calculmental.data.score.Score;
 import com.platydev.calculmental.data.utils.Utils;
 import com.platydev.calculmental.databinding.FragmentEndGameBinding;
 import com.platydev.calculmental.db.ScoreDatabase;
@@ -60,49 +56,67 @@ public class EndGameFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        gameViewModel.isScoreHighScore(ScoreDatabase.getInstance(getContext()), getViewLifecycleOwner());
+        initComponents();
+        setComponentsListener();
+        setObservers();
+    }
+
+    private void initComponents() {
         binding.finalScoreTextView.setText(String.valueOf(gameViewModel.score.getValue()));
         if (gameViewModel.getTimerTime() > 0) {
             binding.finalTimeTextView.setText(Utils.formatTime(gameViewModel.getTimerTime()));
         } else {
             binding.yourTimeTextView.setVisibility(View.INVISIBLE);
         }
+    }
+
+    private void setComponentsListener() {
         binding.welcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager fragmentManager = getParentFragmentManager();
-                for (int i=0; i<fragmentManager.getBackStackEntryCount(); i++) {
-                    fragmentManager.popBackStack();
+                String pseudo = binding.pseudoEditText.getText().toString().trim();
+                if (gameViewModel.saveScore(pseudo, ScoreDatabase.getInstance(getContext()))) {
+                    FragmentManager fragmentManager = getParentFragmentManager();
+                    for (int i=0; i<fragmentManager.getBackStackEntryCount(); i++) {
+                        fragmentManager.popBackStack();
+                    }
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    WelcomeFragment welcomeFragment = WelcomeFragment.newInstance();
+                    fragmentTransaction.replace(R.id.container, welcomeFragment);
+                    fragmentTransaction.commit();
+                } else {
+                    displayErrorDialog();
                 }
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                WelcomeFragment welcomeFragment = WelcomeFragment.newInstance();
-                fragmentTransaction.replace(R.id.container, welcomeFragment);
-                fragmentTransaction.commit();
             }
         });
-        //displayResultDialog();
     }
 
-    private void displayResultDialog() {
+    private void setObservers() {
+        gameViewModel.isHighScore.observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean) {
+                    binding.titleTextView.setTextSize(30);
+                    binding.titleTextView.setText(getResources().getString(R.string.nouveau_meilleur_score));
+                    binding.congratulationsTextView.setText(getResources().getString(R.string.felicitations));
+                    binding.welcomeButton.setText(getResources().getString(R.string.enregistrer));
+                    binding.questionTextView.setVisibility(View.VISIBLE);
+                    binding.pseudoEditText.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private void displayErrorDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setTitle("Bravo! Vous êtes dans le top 10.");
-        builder.setMessage("Votre score est "+ gameViewModel.score.getValue()+".\nEntrez votre pseudo (15 caractères max).");
-
-        final EditText input = new EditText(getContext());
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
+        builder.setTitle("Pseudo incorrect");
+        builder.setMessage("Votre pseudo doit avoir entre 1 et 15 caractères.");
 
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                String pseudo = input.getText().toString().trim();
-                if (!pseudo.isEmpty() && pseudo.length() <= 15) {
-                    Score scoreObj = new Score(pseudo, gameViewModel.score.getValue(),
-                            gameViewModel.getOptions().getNiveau(),
-                            gameViewModel.getTimerTime() != 0 ? gameViewModel.getTimerTime() : gameViewModel.getOptions().getTemps(),
-                            gameViewModel.getOptions().getMode().getNom());
-                    ScoreDatabase.getInstance(getContext()).scoreDAO().insertScore(scoreObj);
-                    dialog.cancel();
-                }
+
             }
         });
         AlertDialog dialog = builder.create();
--- app/src/main/java/com/platydev/calculmental/ui/game/GameViewModel.java
+++ app/src/main/java/com/platydev/calculmental/ui/game/GameViewModel.java
@@ -1,25 +1,33 @@
 package com.platydev.calculmental.ui.game;
 
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.platydev.calculmental.data.gamelogic.ArcadeGameLogic;
 import com.platydev.calculmental.data.gamelogic.CLMGameLogic;
 import com.platydev.calculmental.data.gamelogic.EquationCheckResult;
-import com.platydev.calculmental.data.gamelogic.GameLogicUpdate;
 import com.platydev.calculmental.data.gamelogic.GameLogic;
+import com.platydev.calculmental.data.gamelogic.GameLogicUpdate;
 import com.platydev.calculmental.data.gamelogic.InfiniGameLogic;
 import com.platydev.calculmental.data.gamelogic.ZenGameLogic;
 import com.platydev.calculmental.data.operation.GenAleatoireOperation;
-import com.platydev.calculmental.data.options.Mode;
 import com.platydev.calculmental.data.operation.Operation;
+import com.platydev.calculmental.data.options.Mode;
 import com.platydev.calculmental.data.options.Options;
+import com.platydev.calculmental.data.score.Score;
+import com.platydev.calculmental.db.ScoreDatabase;
+
+import java.util.Collections;
+import java.util.List;
 
 public class GameViewModel extends ViewModel {
 
     MutableLiveData<Integer> score = new MutableLiveData<>();
     MutableLiveData<Operation> currentOperation = new MutableLiveData<>();
     MutableLiveData<Boolean> isFinished = new MutableLiveData<>(false);
+    MutableLiveData<Boolean> isHighScore = new MutableLiveData<>(false);
 
     private long timerTime;
     private boolean timerNeeded = true;
@@ -27,6 +35,8 @@ public class GameViewModel extends ViewModel {
     private boolean chronometer = false;
     private GameLogic logic;
     private Options options;
+    private Score newHighScore;
+    private Score oldHighScore;
 
     public GameViewModel() {
     }
@@ -49,9 +59,12 @@ public class GameViewModel extends ViewModel {
 
     public void init(Options options) {
         this.options = options;
+        newHighScore = null;
+        oldHighScore = null;
 
         currentOperation.postValue(getNewEquation());
         score.postValue(0);
+        isHighScore.postValue(false);
 
         Mode mode = options.getMode();
         switch (mode) {
@@ -103,6 +116,59 @@ public class GameViewModel extends ViewModel {
 
     public void reset() {
         isFinished.postValue(false);
+    }
+
+    public void isScoreHighScore(ScoreDatabase database, LifecycleOwner lifecycleOwner) {
+        if (options.getMode() != Mode.Zen) {
+            Score finalScore = new Score("", score.getValue(), options.getNiveau(),
+                            options.getMode().isTimeLimitedMode() ? options.getTemps() : timerTime,
+                                    options.getMode().getNom());
+            Observer<List<Score>> observer = new Observer<List<Score>>() {
+                @Override
+                public void onChanged(List<Score> scores) {
+                    Collections.sort(scores);
+                    if (scores.isEmpty() || scores.size() < 10) {
+                        newHighScore = finalScore;
+                        isHighScore.postValue(true);
+                    } else if (finalScore.compareTo(scores.get(0)) > 0) {
+                        newHighScore = finalScore;
+                        isHighScore.postValue(true);
+                        oldHighScore = scores.get(0);
+                    }
+                }
+            };
+            if (options.getMode().isTimeLimitedMode()) {
+                database.scoreDAO()
+                        .getScores(options.getNiveau(), options.getMode().getNom(), options.getTemps())
+                        .observe(lifecycleOwner, observer);
+            } else {
+                database.scoreDAO()
+                        .getNotFixedTimeModeScores(options.getNiveau(), options.getMode().getNom())
+                        .observe(lifecycleOwner, observer);
+
+            }
+        }
+
+    }
+
+    public boolean saveScore(String pseudo, ScoreDatabase database) {
+        if (isHighScore.getValue()) {
+            if (!pseudo.isEmpty() && pseudo.length() <= 15) {
+                newHighScore.setPseudo(pseudo);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (oldHighScore != null) {
+                            database.scoreDAO().deleteScore(oldHighScore);
+                        }
+                        database.scoreDAO().insertScore(newHighScore);
+                    }
+                }).start();
+                return true;
+            }
+            return false;
+        }
+        return true;
     }
 
     private void setParameters(boolean chronometer, boolean timelessTimer, boolean timerNeeded, long timerTime) {
