3554ae844d4e67fe2921e8f18a940e0d04e56712
Emmanuel YAO <nehm33@gmail.com>
2024-03-29 18:18:41 +0100

    Finalize scores fragment

--- app/src/main/java/com/platydev/calculmental/ui/scores/ScoresFragment.java
+++ app/src/main/java/com/platydev/calculmental/ui/scores/ScoresFragment.java
@@ -3,24 +3,25 @@ package com.platydev.calculmental.ui.scores;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-
-import com.platydev.calculmental.R;
 import com.platydev.calculmental.data.options.Options;
 import com.platydev.calculmental.data.score.Score;
+import com.platydev.calculmental.data.utils.Utils;
 import com.platydev.calculmental.databinding.FragmentScoresBinding;
 import com.platydev.calculmental.db.ScoreDatabase;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -31,6 +32,13 @@ import java.util.List;
 public class ScoresFragment extends Fragment {
 
     private static final String OPTIONS_PARAM = "options";
+    private static final int RANG_WEIGHT = 1;
+    private static final int PSEUDO_WEIGHT = 3;
+    private static final int SCORE_WEIGHT = 1;
+    private static final int TEMPS_WEIGHT = 2;
+    private static final int DATE_WEIGHT = 3;
+    private static final int HEADER_TEXT_SIZE = 13;
+    private static final int LINE_TEXT_SIZE = 10;
 
     private FragmentScoresBinding binding;
     private Options options;
@@ -67,20 +75,41 @@ public class ScoresFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setTableHeader();
-        setTableLines(ScoreDatabase.getInstance(getContext()).scoreDAO().getScores(options.getNiveau(), options.getMode().getNom(), options.getTemps()));
+
+        Observer<List<Score>> observer = new Observer<List<Score>>() {
+            @Override
+            public void onChanged(List<Score> scores) {
+                if (scores != null) {
+                    Collections.sort(scores);
+                    Collections.reverse(scores);
+                    setTableLines(scores);
+                }
+            }
+        };
+
+        ScoreDatabase database = ScoreDatabase.getInstance(getContext());
+        if (options.getMode().isTimeLimitedMode()) {
+            database.scoreDAO()
+                    .getScores(options.getNiveau(), options.getMode().getNom(), options.getTemps())
+                    .observe(getViewLifecycleOwner(), observer);
+        } else {
+            database.scoreDAO()
+                    .getNotFixedTimeModeScores(options.getNiveau(), options.getMode().getNom())
+                    .observe(getViewLifecycleOwner(), observer);
+        }
     }
 
     private void setTableLines(List<Score> scoreList) {
         for (int i=0; i < scoreList.size(); i++) {
             Score score = scoreList.get(i);
             TableRow line = new TableRow(getContext());
-            line.addView(createTableHeaderTextView(String.valueOf(i+1), 1));
-            line.addView(createTableHeaderTextView(score.getPseudo(), options.getMode().isTimeLimitedMode() ? 3 : 2));
-            line.addView(createTableHeaderTextView(String.valueOf(score.getScore()), 2));
+            line.addView(createTableLineTextView(String.valueOf(i+1), RANG_WEIGHT));
+            line.addView(createTableLineTextView(score.getPseudo(), PSEUDO_WEIGHT));
+            line.addView(createTableLineTextView(String.valueOf(score.getScore()), SCORE_WEIGHT));
             if (!options.getMode().isTimeLimitedMode()) {
-                line.addView(createTableHeaderTextView(String.valueOf(score.getTemps()), 2));
+                line.addView(createTableLineTextView(Utils.formatTime(score.getTemps()), TEMPS_WEIGHT));
             }
-            line.addView(createTableHeaderTextView(score.getDateString(), 3));
+            line.addView(createTableLineTextView(score.getDate(), DATE_WEIGHT));
             binding.table.addView(line);
         }
     }
@@ -88,23 +117,23 @@ public class ScoresFragment extends Fragment {
     private void setTableHeader() {
         TableRow header = new TableRow(getContext());
         header.setBackgroundColor(Color.GREEN);
-        header.addView(createTableLineTextView("Rang", 1));
-        header.addView(createTableLineTextView("Pseudo", options.getMode().isTimeLimitedMode() ? 3 : 2));
-        header.addView(createTableLineTextView("Score", 2));
+        header.addView(createTableHeaderTextView("Rang", RANG_WEIGHT));
+        header.addView(createTableHeaderTextView("Pseudo", PSEUDO_WEIGHT));
+        header.addView(createTableHeaderTextView("Score", SCORE_WEIGHT));
         if (!options.getMode().isTimeLimitedMode()) {
-            header.addView(createTableLineTextView("Temps", 2));
+            header.addView(createTableHeaderTextView("Temps", TEMPS_WEIGHT));
         }
-        header.addView(createTableLineTextView("Date", 3));
+        header.addView(createTableHeaderTextView("Date", DATE_WEIGHT));
         binding.table.addView(header);
     }
 
     private TextView createTableHeaderTextView(String text, int weight) {
         TextView textView = new TextView(getContext());
         textView.setText(text);
-        textView.setTextSize(10);
+        textView.setTextSize(HEADER_TEXT_SIZE);
         textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
         textView.setTextColor(Color.WHITE);
-        textView.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        textView.setGravity(Gravity.CENTER);
         textView.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, weight));
         return textView;
     }
@@ -112,8 +141,8 @@ public class ScoresFragment extends Fragment {
     private TextView createTableLineTextView(String text, int weight) {
         TextView textView = new TextView(getContext());
         textView.setText(text);
-        textView.setTextSize(14);
-        textView.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        textView.setTextSize(LINE_TEXT_SIZE);
+        textView.setGravity(Gravity.CENTER);
         textView.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, weight));
         return textView;
     }
